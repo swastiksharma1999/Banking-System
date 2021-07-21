3257b5deec4d09df61b37162a2aecfb7ec90f515
swastiksharma1999 <87757889+swastiksharma1999@users.noreply.github.com>
2021-07-21 19:20:59 +0530

    Update Indian_bank.sql

--- sql/Indian_bank.sql
+++ sql/Indian_bank.sql
@@ -37,16 +37,16 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
-(1, 'Salman', 'khan@gmail.com', 50000),
-(2, 'Kiara', 'advani@gmail.com', 30000),
+(1, 'Amit', 'khan@gmail.com', 50000),
+(2, 'Amir', 'advani@gmail.com', 30000),
 (3, 'Shahrukh', 'shah@gmail.com', 40000),
 (4, 'Priyanka', 'chopra@gmail.com', 50000),
 (5, 'Shahid', 'kapoor@gmail.com', 40000),
 (6, 'Ranbir', 'singh@gmail.com', 30000),
-(7, 'Deepika', 'padukone@gmail.com', 50000),
-(8, 'Juhi', 'chawla@gmail.com', 40000),
+(7, 'Kartik', 'padukone@gmail.com', 50000),
+(8, 'Hritik', 'chawla@gmail.com', 40000),
 (9, 'Nick', 'jonas@gmail.com', 30000),
-(10, 'Taapsee', 'pannu@gmail.com', 50000);
+(10, 'Akshay', 'pannu@gmail.com', 50000);
 
 --
 -- Indexes for dumped tables
